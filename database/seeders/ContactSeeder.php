8d721c89d6c745007651350f3a41dbff56d06425
issam0999 <isamhajjali@gmail.com>
2025-11-26 14:00:48 +0200

    center admin view

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -14,7 +14,7 @@ class ContactSeeder extends Seeder
     {
         $rows = [
             ['center_id' => 1, 'type_id' => 1, 'name' => 'Issam Hajj Ali', 'email' => 'isamhajjali@gmail.com'], ];
-        DB::table('persons')->insert($rows);
+        DB::table('people')->insert($rows);
         DB::table('users')->update(['person_id' => 1]);
     }
 }
